2206bb78cd2bfafab07e6a9e9f04d2747b9559de
codeway-0841 <joincoin.codeway@gmail.com>
2021-04-28 06:19:58 +0500

    Updates

---
+++ precache-manifest.d726008bfd7fcd885716fd9a532ee068.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8196a51fd641948d8c31ba25b52db0",
+    "url": "/join-frontend/static/media/wave.2b8196a5.svg"
+  },
+  {
+    "revision": "bb6c2eaac5f39c63050d",
+    "url": "/join-frontend/static/css/main.2a171d37.chunk.css"
+  },
+  {
+    "revision": "d18a5d13bcbff96e4050",
+    "url": "/join-frontend/static/js/runtime~main.25721f33.js"
+  },
+  {
+    "revision": "3c841bf605a8a476802d71028127e33b",
+    "url": "/join-frontend/static/media/noreaded.3c841bf6.svg"
+  },
+  {
+    "revision": "deacc85e4b19f6ba15d4",
+    "url": "/join-frontend/static/js/2.24214bdf.chunk.js"
+  },
+  {
+    "revision": "bb6c2eaac5f39c63050d",
+    "url": "/join-frontend/static/js/main.09d6a86e.chunk.js"
+  },
+  {
+    "revision": "65f03140d39415a8814dcce03542c7d3",
+    "url": "/join-frontend/static/media/play.65f03140.svg"
+  },
+  {
+    "revision": "a61933e8724eb3f6190514bdd30baadc",
+    "url": "/join-frontend/static/media/pause.a61933e8.svg"
+  },
+  {
+    "revision": "c1f419d6abb1c32f97508b61040b2074",
+    "url": "/join-frontend/static/media/readed.c1f419d6.svg"
+  },
+  {
+    "revision": "deacc85e4b19f6ba15d4",
+    "url": "/join-frontend/static/css/2.69633163.chunk.css"
+  },
+  {
+    "revision": "b9e46af464aff36bfac7bf64ad5d70f8",
+    "url": "/join-frontend/index.html"
+  }
+];
